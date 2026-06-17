9823330634ecf1ea8be93b98dc624a15fb2f2d73
BosBJJ <samgitrdone@gmail.com>
2026-06-16 20:26:07 -0500

    "first commit"

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/kujtimiihoxha/vimtea"
+
+	main () {
+
+	
+
+	
+
+	
+
+	
+
+	
+	}
+
+
